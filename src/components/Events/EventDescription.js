8a17ed917b64ae98adb5e4fb1f15da5c01d99bb3
Mounvi <mounvip1525@gmail.com>
2021-07-24 23:20:45 +0530

    Initial commit

---
+++ src/components/Events/EventDescription.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function EventDescription() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/containers/Landing.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import HeroBox from '../components/Landing/HeroBox'
+
+export default function Landing() {
+    return (
+        <div>
+            <HeroBox />
+        </div>
+    )
+}
