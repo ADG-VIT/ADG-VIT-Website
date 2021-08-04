b46c4f156a012565593e3275a45bd5a6a09d3b73
Srinivas Varanasi <mr7vsik@gmail.com>
2021-08-04 22:58:10 +0530

    Project added to landing

--- src/containers/Landing.js
+++ src/containers/Landing.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import HeroBox from '../components/Landing/Herobox/HeroBox';
-import Projects from "../components/Projects/Projects";
+
 
 const Landing = () => {
     return (
         <div>
             <HeroBox />
-            <Projects />
         </div>
     )
 }
