39e2be8439811712c86b51a854e444fd5dacd2ee
Kaushal Agrawal <developer.kaushala7@gmail.com>
2021-08-10 18:37:41 +0530

    Add EventsPage

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import Landing from "./containers/Landing";
 import Layout from "./components/Layout/Layout";
-
+import EventsPage from "./components/EventsPage";
 function App() {
   return (
     <div className="App">
       <Switch>
         <Layout />
         <Route exact path="/" component={Landing} />
+        <Route exact path="/events" component={EventsPage} />
       </Switch>
     </div>
   );
