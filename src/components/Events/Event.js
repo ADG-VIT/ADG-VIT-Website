77561ce0f8121e8973a1bdea1b7713178df62a79
Kaushal Agrawal <developer.kaushala7@gmail.com>
2021-08-25 22:14:18 +0530

    Events Page

--- src/components/Events/Event.js
+++ src/components/Events/Event.js
@@ -1,28 +1,46 @@
 import React from "react";
 import "./css/EventsPage.css";
+import Modal from "../Modal";
 
 export default function Event(props) {
-  React.useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
-  return (
-    <div className="tp-item row" style={{ justifyContent: "center" }}>
-      <div className="col-md-5 tp-item-image">
-        <img src={props.img} alt={props.title} className="img-fluid"></img>
+  if (props.width > 768)
+    return (
+      <div className="tp-item row">
+        <div className="col-sm-5 tp-item-image">
+          <img src={props.img} alt={props.title} className="img-fluid"></img>
+        </div>
+        <div className="c-line">
+          <div className="c-circle"></div>
+        </div>
+        <div className="col-sm-5 tp-item-info">
+          <div className="tp-item-title">{props.title}</div>
+          <div className="tp-item-date tp-item-desc">
+            <span>{props.date}</span> <br />
+          </div>
+          <div className="tp-item-desc">
+            <p>{props.desc}</p>
+          </div>
+          <Modal id={props.id}></Modal>
+        </div>
       </div>
-      <div className="c-line">
+    );
+  else
+    return (
+      <div className="l-line">
         <div className="c-circle"></div>
-      </div>
-      <div className="col-md-5 tp-item-info">
-        <div className="tp-item-title">{props.title}</div>
-        <div className="tp-item-date tp-item-desc">
-          <span>{props.date}</span> <br />
+        <div className="tp-item-image">
+          <img src={props.img} alt={props.title} className="img-fluid"></img>
         </div>
-        <div className="tp-item-desc">
-          <p>{props.desc}</p>
+        <div className=" tp-item-info">
+          <div className="tp-item-title">{props.title}</div>
+          <div className="tp-item-date tp-item-desc">
+            {props.date} <br />
+          </div>
+          <div className="tp-item-desc">
+            <p>{props.desc}</p>
+          </div>
+          <Modal id={props.id}></Modal>
         </div>
-        <button className="register-button">Know More</button>
       </div>
-    </div>
-  );
+    );
 }
