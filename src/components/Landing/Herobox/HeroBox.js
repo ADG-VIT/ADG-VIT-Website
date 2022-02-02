f322726318e61436851f6de0d4dbb3950c9e06e5
anmolbansal7 <abanmolbansal5@gmail.com>
2022-02-03 00:43:20 +0530

    minor changes

--- src/components/Landing/Herobox/HeroBox.js
+++ src/components/Landing/Herobox/HeroBox.js
@@ -1,33 +1,36 @@
 import React from "react";
 import {
-  About,
-  ADGLogoWrap,
-  HeroBoxContainer,
-  JoinUsButton,
-  MainHeader,
-  Tagline,
+	About,
+	ADGLogoWrap,
+	HeroBoxContainer,
+	JoinUsButton,
+	MainHeader,
+	Tagline,
 } from "./HeroBoxElements";
 import AdgLogo from "./assets/adglogo.png";
 
 const HeroBox = () => {
-  return (
-    <>
-      <HeroBoxContainer>
-        <ADGLogoWrap>
-          <img src={AdgLogo} alt="ADG Logo" />
-        </ADGLogoWrap>
-        <MainHeader>Apple Developers Group</MainHeader>
-        <Tagline>Tagline</Tagline>
-        <About>
-          Apple Developers Group (ADG) is a name synonymous with excellence,
-          simplicity and dedication. It is a registered student community at
-          VIT, Vellore established under the Apple University program. A coterie
-          of talented minds seeking not just success but perfection.
-        </About>
-        <JoinUsButton to="/">Join us</JoinUsButton>
-      </HeroBoxContainer>
-    </>
-  );
+	return (
+		<>
+			<HeroBoxContainer>
+				<ADGLogoWrap>
+					<img src={AdgLogo} alt="ADG Logo" />
+				</ADGLogoWrap>
+				<MainHeader>Apple Developers Group</MainHeader>
+				<Tagline>Developing your Dream</Tagline>
+				<About>
+					ADGVIT is a student community at VIT that brings together
+					like-minded individuals who are interested in Developing
+					their Dream. The group provides a platform for students to
+					explore their interests while collaborating with others on
+					projects. The aim of the group is to enable students to
+					develop themselves as developers and provide them with all
+					the tools they need for success.
+				</About>
+				<JoinUsButton to="/register">Join us</JoinUsButton>
+			</HeroBoxContainer>
+		</>
+	);
 };
 
 export default HeroBox;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import store from './store/Store'
-import { Provider } from 'react-redux'
+import store from "./store/Store";
+import { Provider } from "react-redux";
+import { HashRouter } from "react-router-dom/cjs/react-router-dom.min";
 
 ReactDOM.render(
-  <Provider store={store}>
-      <App />
-  </Provider>,
-  document.getElementById("root")
+	<Provider store={store}>
+		<HashRouter>
+			<App />
+		</HashRouter>
+	</Provider>,
+	document.getElementById("root")
 );
